4ef6aaa022da803253910ebad9b9f2d3c5e99eb2
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-05-25 10:43:07 -0400

    Update data.py
    add AugTransformMultiClass

--- unet2d/data.py
+++ unet2d/data.py
@@ -40,7 +40,26 @@ class AugTransformConstantPosWeights:
         # reshape
         x_aug = np.expand_dims(x_aug, 0).astype(np.float32)
         y_true_aug = np.expand_dims(y_true_aug.arr[:, :, 0], 0).astype(np.long)
-        weights = self.pos_weight * (y_true_aug != 0).astype(np.float32) + 1
+        weights = self.pos_weight * (y_true_aug > 0).astype(np.float32) + 1
+
+        return {"x": x_aug, "y_true": y_true_aug, "weights": weights}
+
+class AugTransformMultiClass:
+    def __init__(self, aug):
+        self.aug = aug
+
+    def __call__(self, x, y_true):
+        # augment
+        y_true = ia.SegmentationMapsOnImage(y_true, shape=y_true.shape)
+        x_aug, y_true_aug = self.aug(image=x, segmentation_maps=y_true)
+
+        # normalize
+        x_aug = normalize(x_aug)
+
+        # reshape
+        x_aug = np.expand_dims(x_aug, 0).astype(np.float32)
+        y_true_aug = np.expand_dims(y_true_aug.arr[:, :, 0], 0).astype(np.long)
+        weights = (y_true_aug >= 0).astype(np.float32)
 
         return {"x": x_aug, "y_true": y_true_aug, "weights": weights}
 
